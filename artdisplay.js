f69d46e9778971dd050a6b3a4e8f4cb579f231fb
imoran21@gmail.com <imoran21@Gmail.com>
2013-12-25 12:31:28 -0800

    Initial Commit
    One page art display website.  Fluid display with fullsize images upon
    click

---
+++ artdisplay.js
@@ -0,0 +1,315 @@
+$('document').ready(function(){
+  $('.artwrap').hide();
+
+});
+
+$('document').ready(function(){
+  $('.infowrap').hide();
+});
+
+$('document').ready(function(){
+  $('.merchwrap').hide();
+});
+
+
+
+
+$('document').ready(function(){
+      $('#fullimg1').hide();
+  		$('#fullimg2').hide();
+		$('#fullimg3').hide();
+		$('#fullimg4').hide();
+       $('#fullimg5').hide();
+    $('#fullimg6').hide();
+    $('#fullimg7').hide();
+    $('#fullimg8').hide();
+    $('#fullimg9').hide();
+    $('#fullimg10').hide();
+    $('#fullimg11').hide();
+    $('#fullimg12').hide();
+});
+
+
+$('document').ready(function(){
+	$('#artbutton1').click(function(){
+    $("body").scrollTop(0);
+  $('.artwrap').slideToggle('slow');
+  $('.infowrap').slideUp('slow');
+     $('.displaypic').slideDown();
+     $('.merchwrap').slideUp('fast');
+});
+	});
+
+
+$('document').ready(function(){
+	$('#artbutton2').click(function(){
+      $("body").scrollTop(0);
+  $('.infowrap').slideToggle('slow');
+  $('.artwrap').slideUp('slow');
+    
+    $('.merchwrap').slideUp('fast');
+  
+});
+	});
+$('document').ready(function(){
+	$('#artbutton3').click(function(){
+      $("body").scrollTop(0);
+  $('.infowrap').hide('fast');
+  $('.artwrap').hide('fast');
+  $('.merchwrap').slideToggle('fast');
+
+  
+});
+	});
+
+
+
+$('document').ready(function(){
+	$('#fullimg1').hide();
+	$('#imgbox1').click(function(){
+		$('#fullimg2').hide();
+		$('#fullimg3').hide();
+		$('#fullimg4').hide();
+       $('#fullimg5').hide();
+    $('#fullimg6').hide();
+    $('#fullimg7').hide();
+    $('#fullimg8').hide();
+    $('#fullimg9').hide();
+    $('#fullimg10').hide();
+    $('#fullimg11').hide();
+    $('#fullimg12').hide();
+
+
+  $('#fullimg1').slideToggle('fast');
+});
+	});
+
+
+$('document').ready(function(){
+	$('#fullimg2').hide();
+	$('#imgbox2').click(function(){
+		$('#fullimg1').hide();
+		$('#fullimg3').hide();
+		$('#fullimg4').hide();
+    $('#fullimg5').hide();
+    $('#fullimg6').hide();
+    $('#fullimg7').hide();
+    $('#fullimg8').hide();
+      $('#fullimg9').hide();
+    $('#fullimg10').hide();
+    $('#fullimg11').hide();
+    $('#fullimg12').hide();
+
+
+  $('#fullimg2').slideToggle('fast');
+});
+	});
+
+
+
+$('document').ready(function(){
+	$('#fullimg3').hide();
+	$('#imgbox3').click(function(){
+		$('#fullimg1').hide();
+		$('#fullimg2').hide();
+		$('#fullimg4').hide();
+    $('#fullimg5').hide();
+    $('#fullimg6').hide();
+    $('#fullimg7').hide();
+    $('#fullimg8').hide();
+      $('#fullimg9').hide();
+    $('#fullimg10').hide();
+    $('#fullimg11').hide();
+    $('#fullimg12').hide();
+
+
+  $('#fullimg3').slideToggle('fast');
+});
+	});
+
+
+
+$('document').ready(function(){
+	$('#fullimg4').hide();
+	$('#imgbox4').click(function(){
+		$('#fullimg1').hide();
+		$('#fullimg2').hide();
+		$('#fullimg3').hide();
+    $('#fullimg5').hide();
+    $('#fullimg6').hide();
+    $('#fullimg7').hide();
+    $('#fullimg8').hide();
+      $('#fullimg9').hide();
+    $('#fullimg10').hide();
+    $('#fullimg11').hide();
+    $('#fullimg12').hide();
+
+
+  $('#fullimg4').slideToggle('fast');
+});
+	});
+
+$('document').ready(function(){
+	$('#fullimg5').hide();
+	$('#imgbox5').click(function(){
+		$('#fullimg1').hide();
+		$('#fullimg2').hide();
+		$('#fullimg3').hide();
+    $('#fullimg4').hide();
+    $('#fullimg6').hide();
+    $('#fullimg7').hide();
+    $('#fullimg8').hide();
+      $('#fullimg9').hide();
+    $('#fullimg10').hide();
+    $('#fullimg11').hide();
+    $('#fullimg12').hide();
+
+    
+
+$('#fullimg5').slideToggle('fast');
+});
+	});
+
+
+$('document').ready(function(){
+	$('#fullimg6').hide();
+	$('#imgbox6').click(function(){
+		$('#fullimg1').hide();
+		$('#fullimg2').hide();
+		$('#fullimg3').hide();
+    $('#fullimg4').hide();
+    $('#fullimg5').hide();
+    $('#fullimg7').hide();
+    $('#fullimg8').hide();
+      $('#fullimg9').hide();
+    $('#fullimg10').hide();
+    $('#fullimg11').hide();
+    $('#fullimg12').hide();
+
+    
+
+$('#fullimg6').slideToggle('fast');
+});
+	});
+
+
+$('document').ready(function(){
+	$('#fullimg7').hide();
+	$('#imgbox7').click(function(){
+		$('#fullimg1').hide();
+		$('#fullimg2').hide();
+		$('#fullimg3').hide();
+    $('#fullimg4').hide();
+    $('#fullimg5').hide();
+    $('#fullimg6').hide();
+    $('#fullimg8').hide();
+      $('#fullimg9').hide();
+    $('#fullimg10').hide();
+    $('#fullimg11').hide();
+    $('#fullimg12').hide();
+
+    
+
+$('#fullimg7').slideToggle('fast');
+});
+	});
+
+
+$('document').ready(function(){
+	$('#fullimg8').hide();
+	$('#imgbox8').click(function(){
+		$('#fullimg1').hide();
+		$('#fullimg2').hide();
+		$('#fullimg3').hide();
+    $('#fullimg4').hide();
+    $('#fullimg5').hide();
+    $('#fullimg6').hide();
+    $('#fullimg7').hide();
+    $('#fullimg9').hide();
+    $('#fullimg10').hide();
+    $('#fullimg11').hide();
+    $('#fullimg12').hide();
+
+$('#fullimg8').slideToggle('fast');
+});
+	});
+
+$('document').ready(function(){
+	$('#imgbox9').click(function(){
+		$('#fullimg1').hide();
+		$('#fullimg2').hide();
+		$('#fullimg3').hide();
+    $('#fullimg4').hide();
+    $('#fullimg5').hide();
+    $('#fullimg6').hide();
+    $('#fullimg7').hide();
+    $('#fullimg8').hide();
+    $('#fullimg10').hide();
+    $('#fullimg11').hide();
+    $('#fullimg12').hide();
+
+$('#fullimg9').slideToggle('fast');
+});
+	});
+
+$('document').ready(function(){
+	$('#fullimg10').hide();
+	$('#imgbox10').click(function(){
+		$('#fullimg1').hide();
+		$('#fullimg2').hide();
+		$('#fullimg3').hide();
+    $('#fullimg4').hide();
+    $('#fullimg5').hide();
+    $('#fullimg6').hide();
+    $('#fullimg7').hide();
+    $('#fullimg9').hide();
+    $('#fullimg8').hide();
+    $('#fullimg11').hide();
+    $('#fullimg12').hide();
+
+$('#fullimg10').slideToggle('fast');
+});
+	});
+
+
+
+$('document').ready(function(){
+	$('#fullimg11').hide();
+	$('#imgbox11').click(function(){
+		$('#fullimg1').hide();
+		$('#fullimg2').hide();
+		$('#fullimg3').hide();
+    $('#fullimg4').hide();
+    $('#fullimg5').hide();
+    $('#fullimg6').hide();
+    $('#fullimg7').hide();
+    $('#fullimg9').hide();
+    $('#fullimg10').hide();
+    $('#fullimg8').hide();
+    $('#fullimg12').hide();
+
+$('#fullimg11').slideToggle('fast');
+});
+	});
+
+$('document').ready(function(){
+	$('#fullimg12').hide();
+	$('#imgbox12').click(function(){
+		$('#fullimg1').hide();
+		$('#fullimg2').hide();
+		$('#fullimg3').hide();
+    $('#fullimg4').hide();
+    $('#fullimg5').hide();
+    $('#fullimg6').hide();
+    $('#fullimg7').hide();
+    $('#fullimg9').hide();
+    $('#fullimg10').hide();
+    $('#fullimg8').hide();
+    $('#fullimg11').hide();
+
+$('#fullimg12').slideToggle('fast');
+});
+	});
+
+
+
